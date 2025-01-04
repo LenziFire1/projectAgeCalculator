661ab984a4973314bf593126df396cbd2ae1a6dc
LenziFire1 <danhakmen150@gmail.com>
2025-01-04 18:16:34 +0200

    fix bug

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
+let years = document.querySelector(".years")
+let months = document.querySelector(".months")
+let days = document.querySelector(".days")
+let btn = document.querySelector("button")
+let date = document.querySelector(".date")
 
+// console.log(years.innerHTML)
+// console.log(months.innerHTML)
+// console.log(days.innerHTML)
+let currentDate = new Date();
+console.log(currentDate)
+btn.addEventListener('click', function() {
+    let day = date.value.slice(0,2)
+    console.log(day) 
+})
